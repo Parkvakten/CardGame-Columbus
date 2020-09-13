25686cf5ee683112ecac380789f307103ca2ea15
Joakim Vikström <jocke.vikstrom@hotmail.com>
2020-09-13 19:17:22 +0200

    Added Unit Testing

---
+++ TestCardGame/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestCardGame")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestCardGame")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("981f92dd-cf6f-46d5-beb1-721f6dbc8c73")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TestCardGame/UnitTest1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CardGame;
+
+namespace TestCardGame
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        Deck deck = new Deck();
+        
+        [TestMethod]
+        public void TestDeckPull()
+        {
+            Assert.IsTrue(deck.Pull());
+        }
+
+        [TestMethod]
+        public void TestInitializeDeck()
+        {
+            Assert.IsTrue(deck.InitializeDeck());
+        }
+
+        [TestMethod]
+        public void TestSortDeck()
+        {
+            Assert.IsTrue(deck.Sort());
+        }
+
+        [TestMethod]
+        public void TestShuffleDeck()
+        {
+            Assert.IsTrue(deck.Shuffle());
+        }
+    }
+}
